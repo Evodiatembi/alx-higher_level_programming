dc111f050b1a252a245c18c9b91dd89653b1036a
Evodiatembi <evodytembi@gmail.com>
2024-01-16 19:25:21 -0800

    models/square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -30,4 +30,42 @@ class Square(Rectangle):
      def __str__(self):
         """ overloading the str"""
         return "[square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
-     
+
+     def update(self, *args, **kwargs):
+        """
+        assigning arguments to attributes.
+    
+        Args:
+            *args (ints): attributes values.
+                - 1st argument should be the id attribute
+                - 2nd argument should be the size attribute
+                - 3rd argument should be the x attribute
+                - 4th argument should be the y attribute
+            **kwargs (dict): new key/value pairs of attributes.
+        """
+        if args:
+            for count, arg in enumerate(args):
+                if count == 0:
+                    self.id = arg
+                if count == 1:
+                    self.size = arg
+                if count == 2:
+                    self.x = arg
+                if count == 3:
+                    self.y = arg
+                else:
+                    break
+        elif len(kwargs) > 0:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                elif key == "size":
+                    self.width = value
+                    self.height = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
+                else:
+                    break
+
