afaaa0ed7fd3c330c2bc8333b6088a3569b38ca0
Evodiatembi <evodytembi@gmail.com>
2024-01-17 20:34:31 -0800

    base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """defining a base class."""
 
+import json
+
 class Base:
     """creating a class base."""
     __nb_objects = 0
@@ -11,4 +13,14 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects 
 
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        Returns he JSON representation list_dictionaries.
+        Args:
+            list_dictionaries (list): A list of dictionaries.
+        """
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
 
