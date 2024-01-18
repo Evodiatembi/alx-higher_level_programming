5f023ec15029ab8d82a57bf2c4230e8026d9530a
Evodiatembi <evodytembi@gmail.com>
2024-01-17 22:43:51 -0800

    models/base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,7 +2,6 @@
 """defining a base class."""
 
 import json
-
 class Base:
     """creating a class base."""
     __nb_objects = 0
@@ -25,4 +24,23 @@ class Base:
         to_json_string = json.dumps(list_dictionaries)
 
         return to_json_string
+
+    @staticmethod
+    def save_to_file(cls, list_objs):
+        """
+        Returns the JSON representation of list_objs.
+        Args:
+            list_objs (list): A list of instance who inherits of base.
+        """
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as jsonfile:
+            if list_objs is None:
+                jsonfile.write("[]")
+            else:
+                list_dic = []
+                for obj in list_objs:
+                    list_dic.append(obj.to_dictionary())
+                jsonfile.write(Base.to_json_string(list_dic))
+                        
+
 
