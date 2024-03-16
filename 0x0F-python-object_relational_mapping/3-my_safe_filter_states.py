0eb9d0692c4409c95bd3ae2f541e2ab40b0261b5
Evodiatembi <evodytembi@gmail.com>
2024-03-16 13:21:44 -0700

    3-my_safe_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""  script that takes in arguments and displays all values in the states table of hbtn_0e_0_usa"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1], password=sys.argv[2],
+                         db=sys.argv[3],port=3306)
+    cursor = db.cursor()
+    match = state_name=sys.argv[4]
+    cursor.execute("SELECT * FROM states WHERE name LIKE %s", (match, ))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
