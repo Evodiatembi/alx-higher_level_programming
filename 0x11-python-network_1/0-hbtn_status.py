129f63807b55bcb2e39a592998ed3372c74d64f9
Evodiatembi <evodytembi@gmail.com>
2024-03-31 10:28:24 -0700

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,12 +3,10 @@
 fetches https://alx-intranet.hbtn.io/status using the urlib package
 """
 import urllib.request
-if __name__ == '__main__':
-
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     content = response.read()
-    print("body response:"):
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+    print("body response:")
+    print("\n- type: {}".format(type(content)))
+    print("\n- content: {}".format(content))
+    print("\n- utf8 content: {}".format(content.decode('utf-8')))
 
